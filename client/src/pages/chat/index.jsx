6965415e05f2a1970d9a4b4e199d39f369733fe9
Altamash Ahmad <altamashahmad910@gmail.com>
2024-12-16 23:11:42 +0530

    update

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,7 +1,23 @@
+import { useAppStore } from "@/store"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
+
 const Chat = () => {
+
+  const { userInfo } = useAppStore()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(!userInfo.profileSetup){
+      toast("Please Setup The Profile First.")
+      navigate('/profile')
+    }
+  },[userInfo, navigate])
+
   return (
     <div>
-        Chat
+        Chat kaise
     </div>
   )
 }
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -74,3 +74,24 @@ export const login = async (req, res, next) => {
         return res.status(500).json({message: "Error signing up user", error: error.message})
     }
 }
+
+export const getUserInfo = async (req, res, next) => {
+    try {
+        const userData = await User.findById(req.userId);
+        if(!userData) {
+            return res.status(404).json({message: "User not found."});
+        }
+        return res.status(200).json({
+            email: userData.email,
+            id: userData.id,
+            firstName: userData.firstname,
+            lastName: userData.lastname,
+            image: userData.image,
+            profileSetup: userData.profileSetup,
+            color: userData.color
+        })
+    } catch (error) {
+        console.log({error});
+        return res.status(500).json({message: "Error signing up user", error: error.message})
+    }
+}
