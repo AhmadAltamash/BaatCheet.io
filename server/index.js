8935efb1d9c4c2489e9b1848f638bfd6ce5345c9
Altamash Ahmad <altamashahmad910@gmail.com>
2024-12-16 08:56:42 +0530

    update

--- server/index.js
+++ server/index.js
@@ -3,3 +3,29 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost:27017/';
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+mongoose.connect(databaseURL).then(() => console.log(`MongoDB Connected on ${databaseURL}`)).catch((error)=> console.log(error))
+
+app.get('/', (req, res) => {
+    res.send("Hello, Chat App")
+})
