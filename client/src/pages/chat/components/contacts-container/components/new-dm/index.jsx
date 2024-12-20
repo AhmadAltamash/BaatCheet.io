a54f1b5f74b38ceedf5cfa1e36e8dbcd5e0f35fb
Altamash Ahmad <altamashahmad910@gmail.com>
2024-12-20 23:58:31 +0530

    update

--- client/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
+++ client/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
@@ -10,19 +10,34 @@ import {
     DialogDescription,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
   } from "@/components/ui/dialog"
   
 import { useState } from "react"
 import { FaPlus } from "react-icons/fa"
 import { Input } from "@/components/ui/input"
+import { animationDefaultOptions } from "@/lib/utils"
+import Lottie from "react-lottie"
+import { apiClient } from "@/lib/api-client"
+import { SEARCH_CONTACTS_ROUTES } from "@/utils/constants"
   
 const NewDM = () => {
 
     const [openNewContactModel, setOpenNewContactModel] = useState(false)
+    const [searchedContacts, setSearchedContacts] = useState([])
 
-    const searchContacts = async (event) => {
-
+    const searchContacts = async (searchTerm) => {
+        try {
+            if(searchTerm.length > 0){
+                const response = await apiClient.post(SEARCH_CONTACTS_ROUTES, {searchTerm}, {withCredentials: true})
+                if(response.status === 200 && response.data.contacts) {
+                    setSearchedContacts(response.data.contacts)
+                }
+            } else {
+                setSearchedContacts([])
+            }
+        } catch (error) {
+            console.log({ error })
+        }
     }
 
   return (
@@ -40,7 +55,6 @@ const NewDM = () => {
         <Dialog open={openNewContactModel} onOpenChange={setOpenNewContactModel}>
             <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
                 <DialogHeader>
-                {/* <DialogTitle>Are you absolutely sure?</DialogTitle> */}
                 <DialogTitle>Please Select a Contact</DialogTitle>
                 <DialogDescription></DialogDescription>
                 </DialogHeader>
@@ -51,6 +65,19 @@ const NewDM = () => {
                         onChange={(e) => searchContacts(e.target.value)}
                     />
                 </div>
+                {
+                    searchedContacts.length <= 0 &&  <div className="flex-1 bg-transparent md:flex flex-col justify-center items-center duration-1000 transition-all">
+                    <Lottie
+                      isClickToPauseDisabled={true}
+                      height={100}
+                      width={100}
+                      options={animationDefaultOptions}
+                    />
+                    <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
+                      <h3 className="poppins-medium">Hi<span className="text-purple-500">!</span> Search New <span className="text-purple-500">Contact.</span></h3>
+                    </div>
+                  </div>
+                }
             </DialogContent>
         </Dialog>
     </>
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/AuthRoutes.js'
+import contactsRoutes from './routes/ContactRoutes.js'
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use( '/api/auth', authRoutes);
+app.use('/api/contacts', contactsRoutes);
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
