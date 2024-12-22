67dd357b18323b750172988aef6a520301e7ae16
Altamash Ahmad <altamashahmad910@gmail.com>
2024-12-22 15:26:58 +0530

    update

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,3 @@
+import { createContext, useContext, useRef, useEffect } from 'react'
+
+const SocketContext = createContext(null);
--- server/socket.js
+++ server/socket.js
@@ -11,12 +11,27 @@ const setupSocket = (server) => {
 
     const userSocketMap = new Map();
 
+    const disconnect = (socket) => {
+        console.log(`Client disconnected: ${socket.id}`);
+        for(const [userId, socketId] of userSocketMap.entries()) {
+            if(socketId === socket.id) {
+                userSocketMap.delete(userId);
+                break;
+            }
+        }
+    }
+
     io.on("connection", (socket) => {
         const userId = socket.handshake.query.userId;
 
         if(userId) {
             userSocketMap.set(userId, socket.id);
+            console.log(`User Connnected: ${userId} with socket ID: ${socket.id}`);
+        } else {
+            console.log('Unknown user connected');
         }
+
+        socket.on("disconnect", () => disconnect(socket));
     })
 }
 
