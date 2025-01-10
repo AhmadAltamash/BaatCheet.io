10f85eeda6130875b80eeda92c784af18d27060d
Altamash Ahmad <altamashahmad910@gmail.com>
2025-01-10 23:26:19 +0530

    update

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -1,18 +1,20 @@
 import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.jwt;
-    if (!token) {
+  const token = req.cookies.jwt;
+  if (!token) {
       console.log("No token found in cookies");
       return res.status(401).send("You are not authenticated");
-    }
-    jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+  }
+  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
       if (err) {
-        console.log("Invalid token:", err.message);
-        return res.status(403).send("Token is not valid");
+          console.log("Invalid token:", err.message);
+          return res.status(403).send("Token is not valid");
       }
+      console.log("Token is valid, user ID:", payload.userId);
       req.userId = payload.userId;
       next();
-    });
-  };
+  });
+};
+
   
